8c8acb85ff510d8ec242485c8e6788ecc25efa60
Kaylin De Wet <220025754@mycput.ac.za>
2021-04-01 18:18:36 +0200

    first commit

---
+++ src/main/java/za/ac/cput/tdd/App.java
@@ -0,0 +1,9 @@
+package za.ac.cput.tdd;
+
+/**
+ *
+ * @author Kaylin De Wet 220025754
+ */
+public class App {
+    
+}
---
+++ src/test/java/za/ac/cput/tdd/AppTest.java
@@ -0,0 +1,39 @@
+package za.ac.cput.tdd;
+
+//author: Kaylin De Wet 220025754
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+    public AppTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        
+        fail("The test case is a prototype.");
+    }
+    
+}
